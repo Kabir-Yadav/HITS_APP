72389bceba69974dde3452d7a15bc7f92f32e4c4
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2025-03-03 11:39:35 +0530

    Update for nginx reverse proxy

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -20,7 +20,7 @@ export const signInWithGoogle = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/supabase/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
 
