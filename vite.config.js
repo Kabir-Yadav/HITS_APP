e4733726c1a8ac86634362fb7c533c9549c6eea6
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2025-03-10 17:39:46 +0530

    Update vite.config.js for build

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,36 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from 'vite-plugin-eslint';
 import { resolve } from 'path';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.jsx'],
-    }),
-  ],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [
+      react(),
+      eslintPlugin({
+        include: ['src/**/*.js', 'src/**/*.jsx'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0', 
-    port: 8000,  
-    strictPort: true,
-    cors: true
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 8000,
+      strictPort: true,
+      cors: true,
+    },
+    build: {
+      minify: false, 
+      sourcemap: true, 
+      chunkSizeWarningLimit: 1500, 
+    },
+    define: {
+      'process.env': env,
+    },
+  };
 });
