2d53ec9a5ace9110cf4acc3c32c30180a72ded14
Shreyas Ladhe <shreyasladhe.23@gmail.com>
2025-03-11 13:30:46 +0530

    update kanban

--- src/sections/home/view/home-view.jsx
+++ src/sections/home/view/home-view.jsx
@@ -21,6 +21,10 @@ import { HomeHighlightFeatures } from '../home-highlight-features';
 // ----------------------------------------------------------------------
 
 export function HomeView() {
+  const pageProgress = useScrollProgress();
+
+  const { isVisible, onBackToTop }  = useBackToTop();
+
   return (
     <>
       <ScrollProgress
