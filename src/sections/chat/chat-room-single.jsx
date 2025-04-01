67de7844f2dba49571653a2c56942d316d25f816
Yash-Nautiyal <120200862+Yash-Nautiyal@users.noreply.github.com>
2025-04-01 18:45:52 +0530

    added group chat editing dialog

--- src/sections/chat/chat-room-single.jsx
+++ src/sections/chat/chat-room-single.jsx
@@ -1,16 +1,17 @@
-import { useBoolean } from 'minimal-shared/hooks';
+import { useBoolean, usePopover } from 'minimal-shared/hooks';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
 import Collapse from '@mui/material/Collapse';
-import { Button, MenuItem } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { Button, IconButton, MenuItem, MenuList } from '@mui/material';
 
 import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
 
 import { Iconify } from 'src/components/iconify';
+import { CustomPopover } from 'src/components/custom-popover';
 
 import { CollapseButton } from './styles';
 
@@ -20,24 +21,34 @@ export function ChatRoomSingle({ participant }) {
   const collapse = useBoolean(true);
 
   const router = useRouter();
+  const menuActions = usePopover();
+
+  const handleview = () => {
+    router.push(paths.dashboard.user.cards)
+    menuActions.onClose();
+  };
 
   const renderInfo = () => (
-    <Stack alignItems="center" sx={{ pb: 3, pt: 5 }}>
-      <Avatar
-        alt={participant?.name}
-        src={participant?.avatarUrl}
-        sx={{ width: 96, height: 96, mb: 2 }}
-      />
-      <Typography variant="subtitle1">{participant?.name}</Typography>
-      <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5, mb: 2 }}>
-        {participant?.role}
-      </Typography>
-      <Button variant="soft" color="primary"
-        startIcon={<Iconify width={24} icon="solar:user-id-bold" />}
-        onClick={() => router.push(paths.dashboard.user.cards)}>
-        View Profile
-      </Button>
-    </Stack>
+    <Box sx={{ position: 'relative', pb: 3, pt: 5 }}>
+      <IconButton
+        color={menuActions.open ? 'inherit' : 'default'}
+        onClick={menuActions.onOpen}
+        sx={{ position: 'absolute', top: '10%', right: 8, transform: 'rotate(90deg)' }}
+      >
+        <Iconify icon="solar:menu-dots-bold-duotone" />
+      </IconButton>
+      <Stack alignItems="center">
+        <Avatar
+          alt={participant?.name}
+          src={participant?.avatarUrl}
+          sx={{ width: 96, height: 96, mb: 2 }}
+        />
+        <Typography variant="subtitle1">{participant?.name}</Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5, mb: 2 }}>
+          {participant?.role}
+        </Typography>
+      </Stack>
+    </Box>
   );
 
   const renderContact = () => (
@@ -71,6 +82,19 @@ export function ChatRoomSingle({ participant }) {
       </CollapseButton>
 
       <Collapse in={collapse.value}>{renderContact()}</Collapse>
+      <CustomPopover
+        open={menuActions.open}
+        anchorEl={menuActions.anchorEl}
+        onClose={menuActions.onClose}
+        slotProps={{ arrow: { placement: 'right-top' } }}
+      >
+        <MenuList>
+          <MenuItem onClick={handleview}>
+            <Iconify icon="solar:user-circle-bold" />
+            View Profile
+          </MenuItem>
+        </MenuList>
+      </CustomPopover>
     </>
   );
 }
